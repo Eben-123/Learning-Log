0d2b948017bcad721fdc9702cfc281deb3746294
mutabazi-00 <mutajean30@gmail.com>
2024-08-28 17:28:12 +0200

    Ready for deployment to heroku.

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+"""Defines URL patterns for users"""
+
+from django.urls import path, include
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Registration page.
+    path('register/', views.register, name='register'),
+    # Logging out
+    path('logout/', views.logout_view, name='logout'),
+    # Include default auth urls.
+    path('', include('django.contrib.auth.urls')),
+]
